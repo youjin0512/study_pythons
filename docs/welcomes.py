e305d0987b863fcb542e4cafe37caf46ec98ffc9
youjin0512 <kelly2.youjin.kim@gmail.com>
2023-12-04 12:49:22 +0900

    first commit

---
+++ docs/welcomes.py
@@ -0,0 +1,20 @@
+## 변수 선언 후 정의 시 고려점 (넣는 값이 문자 or 숫자)
+# 문자 출력 
+helloworld = "Hello, World! Youjin Kim."  # 문자형 변수
+print(helloworld)
+
+# 숫자 합산 출력
+numbers = 3 + 5  # 숫자형 변수
+print(numbers)
+
+# 데이터 타입 : 문자형 or 숫자형 등 변수에 대한 정의
+
+
+# 가정문
+print(numbers)
+
+if 5 > 2: # 묶음 기호인 :과 tab은 하나의 쌍
+  pass # pass는 디버깅할때 유용
+  print("Five is greater than two!") # ----> 여기까지만 하나의 쌍
+print("end") # ----> 여기는 새로운 문장
+
