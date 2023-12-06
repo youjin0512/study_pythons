0c0069de3f5aab0dbf8342a70c15d774509c0cdd
youjin0512 <kelly2.youjin.kim@gmail.com>
2023-12-06 16:06:34 +0900

    loops_fors

--- docs/data_types_lists.py
+++ docs/data_types_lists.py
@@ -12,12 +12,27 @@ list_fruits = ["apple", "banana", "cherry"]
 # 숫자와 문자 섞은 리스트 -> 가능은 하나, **우린 쓰지 않는다.**
 list_mixs = [0, 1, 2, 3, "apple", "banana"]
 
-pass
-
 # DEBUG COLSOLE
-# len(list_numerics)
+len(list_numerics)
 # 5
-# len(list_mixs)
+len(list_mixs)
 # 6
 
+## for문 활용 후 다시 오기
 
+# index(색인, 위치값)
+list_fruits = ["melon", "apple", "banana", "cherry"]
+## index로 값 가져오기
+list_fruits[0] # 단일 변수로 여김 ; 1차원(행) 단일 변수
+# 'melon'
+list_fruits[3]
+# 'cherry'
+
+## error 발생
+# list_fruits[4]
+# Traceback (most recent call last):
+#   File "<string>", line 1, in <module>
+# IndexError: list index out of range
+
+
+pass
