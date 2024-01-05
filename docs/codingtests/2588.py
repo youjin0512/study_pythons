4fcc342ca78b6df809b23afcdc35f981e94e654f
youjin0512 <kelly2.youjin.kim@gmail.com>
2024-01-05 17:59:52 +0900

    update

--- docs/codingtests/2588.py
+++ docs/codingtests/2588.py
@@ -21,11 +21,21 @@
 
 # <방법 1>
 # : 나머지를 구하는 %을 이용하여 값을 바로 출력하는 방법
+# num1 = int(input())
+# num2 = int(input())
+
+# print(num1 * (num2%10))
+# print(num1 * ((num2%100)//10))
+# print(num1 * (num2//100))
+# print(num1 * num2)
+
+# <방법 2>
+# range(시작, 마지막, 순서) 함수를 이용해 마지막부터 시작까지 역순으로 출력하는 방법
 
 num1 = int(input())
-num2 = int(input())
+num2 = input() 
 
-print(num1 * (num2%10))
-print(num1 * ((num2%100)//10))
-print(num1 * (num2//100))
-print(num1 * num2)
+for i in range(len(num2), 0, -1):	
+    print(num1 * int(num2[i-1]))
+
+print(num1 * int(num2))
